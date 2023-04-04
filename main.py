1931430f100c64a52f0ad21fa8565e8dbd8c2747
viemo777 <viemo777@gmail.com>
2023-04-04 11:43:47 +0300

    users.json

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 import telebot
 from telebot.types import Message
 
@@ -7,20 +9,19 @@ ADMIN_ID = 568869711
 
 @bot_client.message_handler(commands=['start'])
 def start(message: Message):
-
     with open('users.json', 'r') as f:
         data_from_json = json.load(f)
 
     user_id = message.from_user.id
     username = message.from_user.username
-    if user_id not in data_from_json:
-        data_from_json.append()
+    if str(user_id) not in data_from_json:
+        data_from_json[user_id] = {"username": username}
 
-    with open('users.json', 'ц') as f:
-        json.dump(data_from_json, f, indent=4, ensure_ascii=False
+    with open('users.json', 'w') as f:
+        json.dump(data_from_json, f, indent=4, ensure_ascii=False)
 
-
-    bot_client.reply_to(message=message, text=str(message.chat.id))
+    bot_client.reply_to(message=message, text=str(f'Вы зарегистрированы: {username}. '
+                                                  f'Ваш ID: {user_id}'))
 
 
 bot_client.polling()
