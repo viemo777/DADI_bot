fd3a13cc2c466092a7cabb0fdaec930462dce7ce
viemo777 <viemo777@gmail.com>
2023-04-04 11:19:30 +0300

    Initial

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ from telebot.types import Message
 bot_client = telebot.TeleBot(token="6083612607:AAEUMjnLkTX8zzlW6VO2KGRdGxC8JU2KLvE")
 ADMIN_ID = 568869711
 
+
 @bot_client.message_handler(commands=['start'])
 def start(message: Message):
-    bot_client.reply_to(message, "Hello, I am a bot")
+
+    with open('users.json', 'r') as f:
+        data_from_json = json.load(f)
+
+    user_id = message.from_user.id
+    username = message.from_user.username
+    if user_id not in data_from_json:
+        data_from_json.append()
+
+    with open('users.json', 'ц') as f:
+        json.dump(data_from_json, f, indent=4, ensure_ascii=False
+
+
+    bot_client.reply_to(message=message, text=str(message.chat.id))
 
 
 bot_client.polling()
