3475b3725264c8ab6bc9bd579e083aa4a5dcabdd
viemo777 <viemo777@gmail.com>
2023-04-05 18:06:41 +0300

    main

--- main.py
+++ main.py
@@ -3,21 +3,34 @@ from datetime import datetime
 from json import JSONDecodeError
 
 import telebot
-from telebot.types import Message
 from envparse import Env
+from telebot.types import Message
+
+from live import TelegramClient
 
 env = Env()
 TOKEN = env.str('TOKEN')
-ADMIN_ID = env.int('ADMIN_CHAT_ID')
-#add params to env file
-# ADMIN_ID = 568869711
-# TOKEN = "6083612607:AAEUMjnLkTX8zzlW6VO2KGRdGxC8JU2KLvE  "
+ADMIN_CHAT_ID = env.str('ADMIN_CHAT_ID')
+
+
+# add params to env file
+# ADMIN_CHAT_ID = 568869711
+# TOKEN = "6083612607:AAEUMjnLkTX8zzlW6VO2KGRdGxC8JU2KLvE"
 # TOKEN - берется из @BotFather
 
-bot_client = telebot.TeleBot(token=TOKEN)
-@bot_client.message_handler(commands=['start'])
-def start(message: Message):
 
+class MyBot(telebot.TeleBot):
+    def __init__(self, telegram_client: TelegramClient, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.telegram_client = telegram_client
+
+
+telegram_client = TelegramClient(token=TOKEN, base_url='https://api.telegram.org/bot')
+bot = MyBot(token=TOKEN, telegram_client=telegram_client)
+
+
+@bot.message_handler(commands=['start'])
+def start(message: Message):
     with open('users.json', 'r') as f:
         data_from_json = json.load(f)
 
@@ -29,25 +42,32 @@ def start(message: Message):
     with open('users.json', 'w') as f:
         json.dump(data_from_json, f, indent=4, ensure_ascii=False)
 
-    bot_client.reply_to(message=message, text=str(f'Вы зарегистрированы: {username}. '        
-                                                  f'Ваш ID: {user_id}'))
-
-@bot_client.message_handler(commands=['say_speech'])
-def say_speech(message: Message):
-    bot_client.send_message(chat_id=ADMIN_ID, text=str(f'Добрый день. Меня зовут DADIbot. Спросите меня о чем-нибудь'))
+    bot.reply_to(message=message, text=str(f'Вы зарегистрированы: {username}. '
+                                           f'Ваш ID: {user_id}'))
 
 
 def handle_messages(message: Message):
-    bot_client.reply_to(message=message, text=f'У тебя красивое имя, {message.text}!')
-@bot_client.message_handler(commands=['say_speech'])
+    bot.reply_to(message=message, text=f'У тебя красивое имя, {message.text}!')
+
+
+@bot.message_handler(commands=['say_speech'])
 def say_speech(message: Message):
-    bot_client.send_message(message.from_user.id, text='Дoбрый день. Я Vitalii_bot. Назовите ваше имя')
-    bot_client.register_next_step_handler(message, callback=handle_messages)
+    bot.send_message(message.from_user.id, text='Дoбрый день. Я Vitalii_bot. Назовите ваше имя')
+    bot.register_next_step_handler(message, callback=handle_messages)
+
+
+def create_error_message(err: Exception) -> str:
+    return f'Error: {datetime.now()} ::: {err.__class__}: {err}'
+
 
 while True:
     try:
-        bot_client.polling()
+        bot.polling()
     # inform admin about error
     except JSONDecodeError as err:
-        # ADMIN_ID - ID of administator's chat for informing about errors
-        bot_client.send_message(ADMIN_ID, text='Ошибка: ' + f'{datetime.now()} ::: {err.__class__}: {err}')
+        # ADMIN_CHAT_ID - ID of administator's chat for informing about errors.
+        # Вариант записи ошибок в админский телеграмм канал ADMIN_CHAT_ID
+        bot.send_message(ADMIN_CHAT_ID, text='Ошибка: ' + f'{datetime.now()} ::: {err.__class__}: {err}')
+        # альтернативный способ записи ошибки в админсткий телеграмм канал ADMIN_CHAT_ID через post запрос
+        bot.telegram_client.post(method='sendMessage',
+                                 params={'text': 'Error: ' + f'create_error_message(err)', 'chat_id': ADMIN_CHAT_ID})
